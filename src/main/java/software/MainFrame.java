c392edb9c8a8d7f264385781ae3c9708f49b5721
Medichi2002 <pablo.medina108@alu.ulpgc.es>
2024-01-21 17:46:09 +0000

    Fin Kata7

---
+++ src/main/java/software/MainFrame.java
@@ -0,0 +1,14 @@
+package software;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame  extends JFrame {
+
+    public MainFrame() throws HeadlessException{
+        this.setTitle("Kata 7");
+        this.setSize(800,900);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
